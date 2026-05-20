e4e0a2f8966feeb551456b4d6d58c6915edfb6c2
sakeensim <sakeensim@gmail.com>
2026-05-20 23:02:03 +0700

    fix

--- routes/admin-route.js
+++ routes/admin-route.js
@@ -36,4 +36,33 @@ router.get('/admin/dashboard', authenticate, adminAuth, adminController.getEmplo
 
 // router.post('/admin/salary/:employeeId/:year/:month', authenticate,adminController.updateSalaryRecord);
 
+const prisma = require('../configs/prisma')
+
+router.patch('/admin/network-setting', authenticate, adminAuth, async (req, res) => {
+  try {
+
+    const { publicIp } = req.body
+
+    const setting = await prisma.networkSetting.upsert({
+      where: { id: 1 },
+      update: {
+        publicIp
+      },
+      create: {
+        id: 1,
+        name: 'Main Office',
+        publicIp
+      }
+    })
+
+    res.json(setting)
+
+  } catch (error) {
+    console.log(error)
+
+    res.status(500).json({
+      message: 'Server error'
+    })
+  }
+})
 module.exports = router
