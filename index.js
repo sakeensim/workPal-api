56221c039d24092975630f5c2571323d4ec9d0f2
sakeensim <sakeensim@gmail.com>
2026-05-20 21:13:50 +0700

    reroll

--- index.js
+++ index.js
@@ -3,41 +3,32 @@ const cors = require('cors')
 const morgan = require('morgan')
 
 const app = express()
+const handlesErrors = require('.//middleware/error')
 
-const handlesErrors = require('./middleware/error')
-
+//Import Router
 const authRouter = require('./routes/auth-route')
 const userRouter = require('./routes/user-route')
 const adminRouter = require('./routes/admin-route')
-
-const corsOptions = {
-  origin: function (origin, callback) {
-    if (
-      !origin ||
-      origin.includes("vercel.app") ||
-      origin === "http://localhost:5173"
-    ) {
-      callback(null, true)
-    } else {
-      callback(new Error("Not allowed by CORS"))
-    }
-  },
-  credentials: true
-}
-
-app.use(cors(corsOptions))
-
+//midleware
+app.use(cors())
 app.use(morgan('dev'))
-app.use(express.json({ limit: "10mb" }))
+app.use(express.json({limit:"10mb"}))
 
+
+
+// Routing
 app.use('/', authRouter)
 app.use('/', userRouter)
 app.use('/', adminRouter)
 
+
+//Error
 app.use(handlesErrors)
 
-const PORT = process.env.PORT || 9191
 
-app.listen(PORT, () => {
-  console.log(`Server is running on ${PORT}`)
-})
+//Find Not Found
+
+
+
+const PORT = process.env.PORT || 9191
+app.listen(PORT,()=> console.log(`Server is running on ${PORT}`))
