749f49d4e53d302c00922dcec268dc6e1b5e5841
sakeensim <sakeensim@gmail.com>
2026-05-20 20:23:45 +0700

    reroll

--- index.js
+++ index.js
@@ -3,35 +3,35 @@ const cors = require('cors')
 const morgan = require('morgan')
 
 const app = express()
-const handlesErrors = require('.//middleware/error')
 
-//Import Router
+const handlesErrors = require('./middleware/error')
+
 const authRouter = require('./routes/auth-route')
 const userRouter = require('./routes/user-route')
 const adminRouter = require('./routes/admin-route')
-//midleware
-app.use(cors())
+
+const corsOptions = {
+  origin: [
+    "http://localhost:5173",
+    "https://work-pal-web.vercel.app",
+    /\.vercel\.app$/
+  ],
+  credentials: true
+}
 
 app.use(cors(corsOptions))
-app.options("*", cors(corsOptions))
+
 app.use(morgan('dev'))
-app.use(express.json({limit:"10mb"}))
+app.use(express.json({ limit: "10mb" }))
 
-
-
-// Routing
 app.use('/', authRouter)
 app.use('/', userRouter)
 app.use('/', adminRouter)
 
-
-//Error
 app.use(handlesErrors)
 
-
-//Find Not Found
-
-
-
 const PORT = process.env.PORT || 9191
-app.listen(PORT,()=> console.log(`Server is running on ${PORT}`))
+
+app.listen(PORT, () => {
+  console.log(`Server is running on ${PORT}`)
+})
